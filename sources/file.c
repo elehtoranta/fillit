65eb52c3edc6d011b39515cc9501755e34d6ed3d
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2022-06-06 15:53:57 +0300

    Block contact counter fixed from file.c
    Previously the file format checker wasn't
    functioning when the count_contact -function
    was separated as it's own function. The reason was
    that the indexing was applied twice: the buffer
    was referenced to the function by index, and then
    read with that index again inside the function.
    Now (using pointer arithmetic) the function works.

--- sources/file.c
+++ sources/file.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:27:13 by elehtora          #+#    #+#             */
-/*   Updated: 2022/06/06 11:48:25 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/06/06 15:38:47 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ static void	print_piece_r2l(uint16_t piece)
 	}
 }
 
+/*Debugging*/
 /*
- *[>Debugging<]
  *static void	print_piece_l2r(uint16_t piece)
  *{
  *    int	i;
@@ -53,36 +53,6 @@ static void	print_piece_r2l(uint16_t piece)
  *            ft_putendl("");
  *        i--;
  *    }
- *}
- */
-
-/*
- * Find the number of contacting blocks. This function is called for each
- * block individually. The resulting number in the caller has to be
- * 6 OR 8 contacts exactly, or the tetrimino is invalid and the
- * program must terminate with an error.
- *
- * Checking is done clockwise. The first check is for out-of-bounds,
- * the second for the actual hash.
- *
- * This function has separation anxiety, and does not work independently.
- * This issue needs to be sorted.
- */
-/*
- *static int	get_contacts(char *buf, size_t i)
- *{
- *    size_t	contacts;
- *
- *    contacts = 0;
- *    if (i >= 5 && *(buf - 5) == '#') //up
- *        contacts++;
- *    if (i % 5 < 4 && *(buf + 1) == '#') //right
- *        contacts++;
- *    if (i < 15 && *(buf + 5) == '#') //down
- *        contacts++;
- *    if (i % 5 > 0 && *(buf - 1) == '#') //left
- *        contacts++;
- *    return (contacts);
  *}
  */
 
@@ -147,24 +117,26 @@ static int	extract(t_piece *pieces, char *buf, ssize_t ret)
  */
 
 /*
- *static int	count_contacts(const char *buf, size_t i)
- *{
- *    size_t	contacts;
+ * Find the number of contacting blocks. This function is called for each
+ * block individually. The resulting number in the caller has to be
+ * 6 OR 8 contacts exactly, or the tetrimino is invalid and the
+ * program must terminate with an error.
  *
- *    contacts = 0;
- *    ft_putendln(&buf[i], i % 21);
- *    if (i >= 5 && buf[i - 5] == '#') //up
- *        contacts++;
- *    if (i % 5 < 4 && buf[i + 1] == '#') //right
- *        contacts++;
- *    if (i < 15 && buf[i + 5] == '#') //down
- *        contacts++;
- *    if (i % 5 > 0 && buf[i - 1] == '#') //left
- *        contacts++;
- *    ft_putnbr(contacts);
- *    return (contacts);
- *}
+ * Checking is done clockwise. The first check is for out-of-bounds,
+ * the second for the actual hash.
  */
+static int	count_contacts(const char *buf, size_t i, size_t contacts)
+{
+	if (i >= 5 && *(buf - 5) == '#') //up
+		contacts++;
+	if (i % 5 < 4 && *(buf + 1) == '#') //right
+		contacts++;
+	if (i < 15 && *(buf + 5) == '#') //down
+		contacts++;
+	if (i % 5 > 0 && *(buf - 1) == '#') //left
+		contacts++;
+	return (contacts);
+}
 
 static int	is_good_piece(char *buf, size_t hashes, size_t contacts)
 {
@@ -181,15 +153,7 @@ static int	is_good_piece(char *buf, size_t hashes, size_t contacts)
 		if (buf[i] == '#')
 		{
 			hashes++;
-			if (i >= 5 && buf[i - 5] == '#') //up
-				contacts++;
-			if (i % 5 < 4 && buf[i + 1] == '#') //right
-				contacts++;
-			if (i < 15 && buf[i + 5] == '#') //down
-				contacts++;
-			if (i % 5 > 0 && buf[i - 1] == '#') //left
-				contacts++;
-			/*contacts += count_contacts(&buf[i], i);*/ // for god knows why this doesn't work
+			contacts += count_contacts(&buf[i], i, 0);
 		}
 		i++;
 	}
@@ -217,19 +181,6 @@ static int	validate_pieces(char *buf, size_t ret)
 	return (0);
 }
 
-/*
- *static int	verify(char *buf, ssize_t ret)
- *{
- *    if (ret > MAX_READ)
- *        return (error(FILE_MAX));
- *    if ((ret + 1) % 21 != 0)
- *        return (error(FILE_FORMAT));
- *    if (validate_pieces(&buf[0], ret) == -1)
- *        return (-1);
- *    return (0);
- *}
- */
-
 int	extract_file(const char *file, t_piece *pieces)
 {
 	static char	buf[MAX_READ + 1];
@@ -245,10 +196,6 @@ int	extract_file(const char *file, t_piece *pieces)
 	if (close(fd) == -1)
 		return (error(CLOSE_FAIL));
 	buf[ret] = 0;
-	/*
-	 *if (verify(&buf[0], ret) == -1)
-	 *    return (-1);
-	 */
 	if (ret > MAX_READ)
 		return (error(FILE_MAX));
 	if ((ret + 1) % 21 != 0)
