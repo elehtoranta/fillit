1a9b1c67c36e0073bcef02beb49ac9c535336d10
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2022-06-06 11:55:30 +0300

    Refactored format detection
    Previous iteration was not checking
    the pieces adequetly, and when that
    checking was added, a few of the previous
    checks became redundant. Those were refactored
    out.

--- sources/file.c
+++ sources/file.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:27:13 by elehtora          #+#    #+#             */
-/*   Updated: 2022/06/06 09:34:48 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/06/06 11:48:25 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,9 @@ static void	print_piece_r2l(uint16_t piece)
  *
  * Checking is done clockwise. The first check is for out-of-bounds,
  * the second for the actual hash.
+ *
+ * This function has separation anxiety, and does not work independently.
+ * This issue needs to be sorted.
  */
 /*
  *static int	get_contacts(char *buf, size_t i)
@@ -91,7 +94,7 @@ static int	set_piece(t_piece *piece, char *buf, int id)
 
 	i = 0;
 	row = 0;
-	/*Check for piece delimiter in caller*/
+	/*Debugging*/
 	/*
 	 *ft_putchar(buf[i]);
 	 *ft_putendl(" at beginning of set_piece");
@@ -100,12 +103,10 @@ static int	set_piece(t_piece *piece, char *buf, int id)
 	{
 		/*Debugging*/
 		/*ft_putchar(buf[i]); // printing pieces from chars*/
-		/*if (i % 4 == 0 && i != 0)*/
 		if (buf[i] == '\n' && i > 0)
 			row++;
 		if (buf[i] == '#')
 			piece->piece |= 1 << (i - row);
-			/*piece->piece |= 1 << ((PIECE_BLOCKS - 1) - i + row);*/
 		i++;
 	}
 	/*Debugging*/
@@ -118,7 +119,6 @@ static int	set_piece(t_piece *piece, char *buf, int id)
 
 static int	extract(t_piece *pieces, char *buf, ssize_t ret)
 {
-
 	size_t	total_pieces;
 	size_t	nth;
 
@@ -128,6 +128,7 @@ static int	extract(t_piece *pieces, char *buf, ssize_t ret)
 	{
 		if (set_piece(&pieces[nth], &buf[nth * PIECE_READ], nth) == -1)
 			return (-1);
+		/*Debugging*/
 		print_piece_r2l(pieces[nth].piece);
 		ft_putnbr(pieces[nth].piece);
 		ft_putendl("");
@@ -136,13 +137,22 @@ static int	extract(t_piece *pieces, char *buf, ssize_t ret)
 	return (0);
 }
 
+/*Print n characters from string str, ending in newline*/
 /*
- *static int	count_contacts(char *buf, size_t i)
+ *static void	ft_putendln(const char *str, size_t n)
+ *{
+ *    write(1, str, n);
+ *    write(1, "\n", 1);
+ *}
+ */
+
+/*
+ *static int	count_contacts(const char *buf, size_t i)
  *{
  *    size_t	contacts;
  *
  *    contacts = 0;
- *    ft_putchar(buf[i - 5]);
+ *    ft_putendln(&buf[i], i % 21);
  *    if (i >= 5 && buf[i - 5] == '#') //up
  *        contacts++;
  *    if (i % 5 < 4 && buf[i + 1] == '#') //right
@@ -151,7 +161,6 @@ static int	extract(t_piece *pieces, char *buf, ssize_t ret)
  *        contacts++;
  *    if (i % 5 > 0 && buf[i - 1] == '#') //left
  *        contacts++;
- *    ft_putnbr(i);
  *    ft_putnbr(contacts);
  *    return (contacts);
  *}
@@ -180,7 +189,7 @@ static int	is_good_piece(char *buf, size_t hashes, size_t contacts)
 				contacts++;
 			if (i % 5 > 0 && buf[i - 1] == '#') //left
 				contacts++;
-			/*contacts += count_contacts(&buf[i], i);*/
+			/*contacts += count_contacts(&buf[i], i);*/ // for god knows why this doesn't work
 		}
 		i++;
 	}
@@ -193,64 +202,33 @@ static int	is_good_piece(char *buf, size_t hashes, size_t contacts)
 
 static int	validate_pieces(char *buf, size_t ret)
 {
-	ssize_t	i;
+	size_t	i;
 
 	i = 0;
-	while ((size_t)i < ret)
+	while (i < ret)
 	{
-		if (is_good_piece(&buf[i], 0, 0 && buf[i + 1] == '\n') == -1)
+		if (is_good_piece(&buf[i], 0, 0) == -1)
 				return (-1);
 		i += PIECE_READ;
-		/*
-		 *if (buf[i] != '\n' && (size_t)i < ret && i > 0)
-		 *    return (error(MISSING_NEWLINE));
-		 */
+		/*checking 20th index 'per piece' for separating newline*/
+		if (buf[i - 1] != '\n' && i < ret && i > 0)
+			return (error(MISSING_NEWLINE));
 	}
 	return (0);
 }
 
 /*
- *static int	invalid_chars(char *buf, ssize_t ret)
+ *static int	verify(char *buf, ssize_t ret)
  *{
- *    size_t	i;
- *
- *    i = 0;
- *    while (buf[i] == '.' || buf[i] == '\n' || buf[i] == '#')
- *    {
- *        if (i + 1 % PIECE_READ == 0 && buf[i] != '\n' && i < (size_t)ret)
- *            return(error(MISSING_NEWLINE));
- *        i++;
- *    }
- *    if (i != (size_t)ret)
- *        return (error(INVALID_CHARS));
- *    else if (hashes != ((size_t)ret + 1) / 21 * 4)
- *        return (error(BAD_HASH_COUNT));
- *    else
- *        return (0);
+ *    if (ret > MAX_READ)
+ *        return (error(FILE_MAX));
+ *    if ((ret + 1) % 21 != 0)
+ *        return (error(FILE_FORMAT));
+ *    if (validate_pieces(&buf[0], ret) == -1)
+ *        return (-1);
+ *    return (0);
  *}
  */
-
-static int	verify(char *buf, ssize_t ret)
-{
-	size_t	valid;
-
-	valid = 0;
-	if (ret > MAX_READ)
-		return (error(FILE_MAX));
-	if ((ret + 1) % 21 != 0)
-		return (error(FILE_FORMAT));
-	if (validate_pieces(&buf[valid], ret) == -1)
-		return (-1);
-	/*
-	 *if (validate_pieces(&buf[0], ret) == -1)
-	 *    return (-1);
-	 */
-	/*
-	 *if (invalid_chars(&buf[0], ret))
-	 *    return (-1);
-	 */
-	return (0);
-}
 
 int	extract_file(const char *file, t_piece *pieces)
 {
@@ -267,7 +245,15 @@ int	extract_file(const char *file, t_piece *pieces)
 	if (close(fd) == -1)
 		return (error(CLOSE_FAIL));
 	buf[ret] = 0;
-	if (verify(&buf[0], ret) == -1)
+	/*
+	 *if (verify(&buf[0], ret) == -1)
+	 *    return (-1);
+	 */
+	if (ret > MAX_READ)
+		return (error(FILE_MAX));
+	if ((ret + 1) % 21 != 0)
+		return (error(FILE_FORMAT));
+	if (validate_pieces(&buf[0], ret) == -1)
 		return (-1);
 	if (extract(pieces, &buf[0], ret) == -1)
 		return (-1);
